968c930ec12351a1b19c0048001bfde014b8bffb
beimnetwondu <154693619+beimnetwondu@users.noreply.github.com>
2023-12-23 18:54:06 +0100

    Create convert the temperature.cpp

---
+++ convert the temperature.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<double> convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return {kelvin, fahrenheit};
+    }
+};
